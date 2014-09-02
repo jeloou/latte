e912f206a362f5be0456e1dfe862ba008b24f2ca
Joseph Núñez <toctochello@gmail.com>
2014-09-01 23:05:45 -0430

    utils: taking const arrays, adding char_in_array

--- src/utils/common.c
+++ src/utils/common.c
@@ -1,8 +1,11 @@
+#include <string.h>
+#include <sys/types.h>
+
 #include "utils/common.h"
 
 int 
-str_in_array(char *s, char *a[], size_t size) {
-  int i = 0;
+str_in_array(char *s, const char *a[], size_t size) {
+  unsigned int i = 0;
   
   while (i < size) {
     if (strcmp(s, a[i]) == 0) { 
@@ -14,12 +17,26 @@ str_in_array(char *s, char *a[], size_t size) {
   return -1;
 }
 
-int
-int_in_array(int n, int a[], size_t size) {
-  int i = 0;
+int 
+int_in_array(int n, const int a[], size_t size) {
+  unsigned int i = 0;
   
   while (i < size) {
     if (n == a[i]) { 
+      return i;
+    }
+    ++i;
+  }
+  
+  return -1;
+}
+
+int 
+char_in_array(char c, const char a[], size_t size) {
+  unsigned int i = 0;
+  
+  while (i < size) {
+    if (c == a[i]) { 
       return i;
     }
     ++i;
--- src/utils/common.h
+++ src/utils/common.h
@@ -3,7 +3,8 @@
 
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof(*a))
 
-int str_in_array(char *s, char *a[], size_t size);
-int int_in_array(int n, int a[], size_t size);
+int str_in_array(char *s, const char *a[], size_t size);
+int int_in_array(int n, const int a[], size_t size);
+int char_in_array(char n, const char a[], size_t size);
 
 #endif
